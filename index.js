c41dc147a21dd3928c4aa062daeccc540472df1c
Brad Anderson <banderso@nerdery.com>
2021-09-13 16:15:10 -0400

    Adds addTodo and updateTodo mutations

--- index.js
+++ index.js
@@ -1,30 +1,66 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql, UserInputError } = require('apollo-server');
 
 const typeDefs = gql`
 type Todo {
-    text: String
+    id: ID!
+    text: String!
     complete: Boolean!
 }
 
 type Query {
     todos: [Todo]
 }
+
+input TodoInput {
+    text: String!
+    complete: Boolean
+}
+
+type Mutation {
+    addTodo(input: TodoInput!): Todo
+    updateTodo(id: ID!, input: TodoInput!): Todo
+}
 `;
+
+let i = 0;
+const id = () => {
+    i++;
+    return String(i);
+};
 
 const todos = [
     {
-      text: 'one',
-      complete: true,
+        id: id(),
+        text: 'one',
+        complete: true,
     },
     {
-      text: 'two',
-      complete: false,
+        id: id(),
+        text: 'two',
+        complete: false,
     },
 ];
 
 const resolvers = {
     Query: {
         todos: () => todos,
+    },
+    Mutation: {
+        addTodo: (_, { input }) => {
+            const newTodo = { id: id(), complete: false, ...input };
+            todos.push(newTodo);
+            return newTodo;
+        },
+        updateTodo: (_, { id, input }) => {
+            const index = todos.findIndex(x => x.id === id);
+            const todo = todos[index];
+            if (!todo) {
+                throw new UserInputError(`Could not find todo with id: ${id}`);
+            }
+            const updatedTodo = { ...todo, ...input };
+            todos.splice(index, 1, updatedTodo);
+            return updatedTodo;
+        },
     },
 };
 
